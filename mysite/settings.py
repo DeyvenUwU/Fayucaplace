717b417c5ed3bf0b1d6feb5505465235c8a6d416
AlejandroLT01 <s22120237@alumnos.itsur.edu.mx>
2025-12-02 03:06:57 -0600

    Cambios en settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-dev-key')
 DEBUG = True   # seguimos en desarrollo
 
 # ✔ Permite ngrok automáticamente
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok-free.dev']
 
 # ✔ Acepta CSRF desde ngrok
 CSRF_TRUSTED_ORIGINS = [
