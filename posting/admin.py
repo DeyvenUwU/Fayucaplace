60bb0717b3ba8de0a10f04de1c2a6cd70fce55d8
AlejandroLT01 <s22120237@alumnos.itsur.edu.mx>
2025-12-03 16:25:55 -0600

    correccion de errores 3

--- posting/admin.py
+++ posting/admin.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 from django.contrib import admin
 from .models import *
 
@@ -9,21 +8,3 @@ admin.site.register(Articulo)
 admin.site.register(Anuncio)
 admin.site.register(Categoria)
 admin.site.register(SubCategoria)
-=======
-from django.contrib import admin
-from .models import Publicacion, Articulo, Anuncio
-
-# Register your models here.
-admin.site.register(Publicacion)
-admin.site.register(Articulo)
-admin.site.register(Anuncio)
->>>>>>> f271bf24dfb35a8596c07af58ada957384fefd36
-=======
-from django.contrib import admin
-from .models import Publicacion, Articulo, Anuncio
-
-# Register your models here.
-admin.site.register(Publicacion)
-admin.site.register(Articulo)
-admin.site.register(Anuncio)
->>>>>>> f271bf24dfb35a8596c07af58ada957384fefd36
